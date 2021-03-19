b1cc78a067d741cb1d2cb7d03a283c2657e35f88
Alexandre Wolff <alexandrewlff@gmail.com>
2021-03-19 17:03:41 +0100

    Fix Content component display bug

--- frontend/src/components/Client/Editor/Content/Content.js
+++ frontend/src/components/Client/Editor/Content/Content.js
@@ -5,18 +5,18 @@ import './Content.scss'
 const Content = ({ note, titleInputHandler, contentInputHandler }) => {
   return (
     <div className="content">
-      <h2>
+      <h2 className="content__title">
         <input
           type="text"
-          className="content__title"
+          className="content__title-field"
           value={note.title}
           placeholder="Title"
           onChange={titleInputHandler}
         />
       </h2>
-      <p className="content__paragraph">
+      <p className="content__text">
         <textarea
-          className="content__text"
+          className="content__text-field"
           value={note.content}
           placeholder="Text"
           onChange={contentInputHandler}
